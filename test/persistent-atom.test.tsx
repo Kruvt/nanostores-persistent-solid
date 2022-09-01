0779a7d71baa4e9886bb343aba1915174be31f06
vxncetxn <thevancetan@gmail.com>
2022-09-01 21:52:47 +0800

    Mostly completed tests for persistent atom and map

--- test/persistent-atom.test.tsx
+++ test/persistent-atom.test.tsx
@@ -1,76 +1,80 @@
 import { afterEach, expect, it } from 'vitest';
 import { cleanup, render, screen } from 'solid-testing-library';
-import { delay } from 'nanodelay';
 import { action } from 'nanostores';
 import { persistentAtom } from '../src/persistent';
 import { useStore } from '../src/use-store';
 
 afterEach(() => {
   cleanup();
+  localStorage.clear();
 });
 
-it('Initializes data in localStorage and renders it', async () => {
-  const item = persistentAtom('test:key', 'initialValue');
+it('Initializes data in localStorage and renders it if no existing data present in localStorage', async () => {
+  const atom = persistentAtom('test:key', 'initialValue');
 
   const Wrapper = () => {
-    const $item = useStore(item);
+    const $atom = useStore(atom);
 
-    return <div data-testid="test-1">{$item()}</div>;
+    return <div data-testid="div-1">{$atom()}</div>;
   };
 
+  expect(localStorage.getItem('test:key')).toEqual(null);
   render(() => <Wrapper />);
-  expect(screen.getByTestId('test-1')).toHaveTextContent('initialValue');
+  expect(screen.getByTestId('div-1')).toHaveTextContent('initialValue');
   expect(localStorage.getItem('test:key')).toEqual('initialValue');
 });
 
 it('Mutates data and then saves to localStorage and renders it', async () => {
-  const item = persistentAtom('test:key', 'initialValue');
-  const mutate = action(item, 'mutate', (store, value) => store.set(value));
+  const atom = persistentAtom('test:key', 'initialValue');
+  const mutate = action(atom, 'mutate', (store, value) => store.set(value));
 
   const Wrapper = () => {
-    const $item = useStore(item);
+    const $atom = useStore(atom);
 
     return (
       <>
-        <div data-testid="test-2">{$item()}</div>
+        <div data-testid="div-1">{$atom()}</div>
         <button onClick={() => mutate('newValue')}>Mutate</button>
       </>
     );
   };
 
   render(() => <Wrapper />);
+  expect(screen.getByTestId('div-1')).toHaveTextContent('initialValue');
+  expect(localStorage.getItem('test:key')).toEqual('initialValue');
   screen.getByRole('button').click();
-  expect(screen.getByTestId('test-2')).toHaveTextContent('newValue');
+  expect(screen.getByTestId('div-1')).toHaveTextContent('newValue');
   expect(localStorage.getItem('test:key')).toEqual('newValue');
 });
 
 it('Loads data from localStorage and renders it', async () => {
-  const item = persistentAtom('test:key', 'initialValue');
+  const atom = persistentAtom('test:key', 'initialValue');
 
   localStorage.setItem('test:key', 'savedValue');
 
   const Wrapper = () => {
-    const $item = useStore(item);
+    const $atom = useStore(atom);
 
-    return <div data-testid="test-3">{$item()}</div>;
+    return <div data-testid="div-1">{$atom()}</div>;
   };
 
   render(() => <Wrapper />);
-  expect(screen.getByTestId('test-3')).toHaveTextContent('savedValue');
+  expect(localStorage.getItem('test:key')).toEqual('savedValue');
+  expect(screen.getByTestId('div-1')).toHaveTextContent('savedValue');
 });
 
 it('Reverts back to initial data if keys manually cleaned in localStorage and renders it', async () => {
-  const item = persistentAtom('test:key', 'initialValue');
+  const atom = persistentAtom('test:key', 'initialValue');
 
   localStorage.clear();
 
   const Wrapper = () => {
-    const $item = useStore(item);
-    const mutate = action(item, 'mutate', (store, value) => store.set(value));
+    const $atom = useStore(atom);
+    const mutate = action(atom, 'mutate', (store, value) => store.set(value));
 
     return (
       <>
-        <div data-testid="test-4">{$item()}</div>
+        <div data-testid="div-1">{$atom()}</div>
         <button data-testid="button-1" onClick={() => mutate('newValue')}>
           Mutate
         </button>
@@ -83,6 +87,75 @@ it('Reverts back to initial data if keys manually cleaned in localStorage and re
 
   render(() => <Wrapper />);
   screen.getByTestId('button-1').click();
+  expect(localStorage.getItem('test:key')).toEqual('newValue');
+  expect(screen.getByTestId('div-1')).toHaveTextContent('newValue');
   screen.getByTestId('button-2').click();
-  expect(screen.getByTestId('test-4')).toHaveTextContent('initialValue');
+  expect(localStorage.getItem('test:key')).toEqual(null);
+  expect(screen.getByTestId('div-1')).toHaveTextContent('initialValue');
+});
+
+it('Updates data by listening to other tabs and renders it', async () => {
+  const atom = persistentAtom('test:key', 'initialValue');
+
+  const Wrapper = () => {
+    const $atom = useStore(atom);
+
+    return (
+      <>
+        <div data-testid="div-1">{$atom()}</div>
+        <button
+          onClick={() => {
+            window.dispatchEvent(
+              new StorageEvent('storage', {
+                key: 'test:key',
+                newValue: 'emittedValue'
+              })
+            );
+          }}
+        >
+          Emit
+        </button>
+      </>
+    );
+  };
+
+  render(() => <Wrapper />);
+  expect(localStorage.getItem('test:key')).toEqual('initialValue');
+  expect(screen.getByTestId('div-1')).toHaveTextContent('initialValue');
+  screen.getByRole('button').click();
+  expect(localStorage.getItem('test:key')).toEqual('initialValue');
+  expect(screen.getByTestId('div-1')).toHaveTextContent('emittedValue');
+});
+
+it('Should not update data by listening to other tabs if disabled and continues rendering existing data', async () => {
+  const atom = persistentAtom('test:key', 'initialValue', { listen: false });
+
+  const Wrapper = () => {
+    const $atom = useStore(atom);
+
+    return (
+      <>
+        <div data-testid="div-1">{$atom()}</div>
+        <button
+          onClick={() => {
+            window.dispatchEvent(
+              new StorageEvent('storage', {
+                key: 'test:key',
+                newValue: 'emittedValue'
+              })
+            );
+          }}
+        >
+          Emit
+        </button>
+      </>
+    );
+  };
+
+  render(() => <Wrapper />);
+  expect(localStorage.getItem('test:key')).toEqual('initialValue');
+  expect(screen.getByTestId('div-1')).toHaveTextContent('initialValue');
+  screen.getByRole('button').click();
+  expect(localStorage.getItem('test:key')).toEqual('initialValue');
+  expect(screen.getByTestId('div-1')).toHaveTextContent('initialValue');
 });
---
+++ test/persistent-map.test.tsx
@@ -0,0 +1,239 @@
+import { afterEach, expect, it } from 'vitest';
+import { cleanup, render, screen } from 'solid-testing-library';
+import { action } from 'nanostores';
+import { persistentMap } from '../src/persistent';
+import { useStore } from '../src/use-store';
+
+afterEach(() => {
+  cleanup();
+  localStorage.clear();
+});
+
+it('Initializes data in localStorage and renders it if no existing data present in localStorage', async () => {
+  const testMap = persistentMap('test:', {
+    keyOne: 'initialValueOne',
+    keyTwo: 'initialValueTwo'
+  });
+
+  const Wrapper = () => {
+    const $testMap = useStore(testMap);
+
+    return (
+      <>
+        <div data-testid="div-1">{$testMap().keyOne}</div>
+        <div data-testid="div-2">{$testMap().keyTwo}</div>
+      </>
+    );
+  };
+
+  expect(localStorage.getItem('test:keyOne')).toEqual(null);
+  expect(localStorage.getItem('test:keyTwo')).toEqual(null);
+  render(() => <Wrapper />);
+  expect(screen.getByTestId('div-1')).toHaveTextContent('initialValueOne');
+  expect(screen.getByTestId('div-2')).toHaveTextContent('initialValueTwo');
+  expect(localStorage.getItem('test:keyOne')).toEqual('initialValueOne');
+  expect(localStorage.getItem('test:keyTwo')).toEqual('initialValueTwo');
+});
+
+it('Mutates data and then saves to localStorage and renders it', async () => {
+  const testMap = persistentMap('test:', {
+    keyOne: 'initialValueOne',
+    keyTwo: 'initialValueTwo'
+  });
+  const mutateOne = action(testMap, 'mutate-one', (store, value) =>
+    store.setKey('keyOne', value)
+  );
+  const mutateTwo = action(testMap, 'mutate-two', (store, value) =>
+    store.setKey('keyTwo', value)
+  );
+
+  const Wrapper = () => {
+    const $testMap = useStore(testMap);
+
+    return (
+      <>
+        <div data-testid="div-1">{$testMap().keyOne}</div>
+        <div data-testid="div-2">{$testMap().keyTwo}</div>
+        <button onClick={() => mutateOne('newValueOne')}>Mutate One</button>
+        <button onClick={() => mutateTwo('newValueTwo')}>Mutate Two</button>
+      </>
+    );
+  };
+
+  render(() => <Wrapper />);
+  expect(screen.getByTestId('div-1')).toHaveTextContent('initialValueOne');
+  expect(screen.getByTestId('div-2')).toHaveTextContent('initialValueTwo');
+  expect(localStorage.getItem('test:keyOne')).toEqual('initialValueOne');
+  expect(localStorage.getItem('test:keyTwo')).toEqual('initialValueTwo');
+  screen.getAllByRole('button').forEach(button => button.click());
+  expect(screen.getByTestId('div-1')).toHaveTextContent('newValueOne');
+  expect(screen.getByTestId('div-2')).toHaveTextContent('newValueTwo');
+  expect(localStorage.getItem('test:keyOne')).toEqual('newValueOne');
+  expect(localStorage.getItem('test:keyTwo')).toEqual('newValueTwo');
+});
+
+it('Loads data from localStorage and renders it', async () => {
+  const testMap = persistentMap('test:', {
+    keyOne: 'initialValueOne',
+    keyTwo: 'initialValueTwo'
+  });
+
+  localStorage.setItem('test:keyOne', 'savedValueOne');
+  localStorage.setItem('test:keyTwo', 'savedValueTwo');
+
+  const Wrapper = () => {
+    const $testMap = useStore(testMap);
+
+    return (
+      <>
+        <div data-testid="div-1">{$testMap().keyOne}</div>
+        <div data-testid="div-2">{$testMap().keyTwo}</div>
+      </>
+    );
+  };
+
+  render(() => <Wrapper />);
+  expect(localStorage.getItem('test:keyOne')).toEqual('savedValueOne');
+  expect(localStorage.getItem('test:keyTwo')).toEqual('savedValueTwo');
+  expect(screen.getByTestId('div-1')).toHaveTextContent('savedValueOne');
+  expect(screen.getByTestId('div-2')).toHaveTextContent('savedValueTwo');
+});
+
+it('Loads partially-present data from localStorage, and initializes absent data with initial values, and renders them', async () => {
+  const testMap = persistentMap('test:', {
+    keyOne: 'initialValueOne',
+    keyTwo: 'initialValueTwo'
+  });
+
+  localStorage.setItem('test:keyOne', 'savedValueOne');
+  localStorage.removeItem('test:keyTwo');
+
+  const Wrapper = () => {
+    const $testMap = useStore(testMap);
+
+    return (
+      <>
+        <div data-testid="div-1">{$testMap().keyOne}</div>
+        <div data-testid="div-2">{$testMap().keyTwo}</div>
+      </>
+    );
+  };
+
+  expect(localStorage.getItem('test:keyOne')).toEqual('savedValueOne');
+  expect(localStorage.getItem('test:keyTwo')).toEqual(null);
+  render(() => <Wrapper />);
+  expect(localStorage.getItem('test:keyOne')).toEqual('savedValueOne');
+  expect(localStorage.getItem('test:keyTwo')).toEqual('initialValueTwo');
+  expect(screen.getByTestId('div-1')).toHaveTextContent('savedValueOne');
+  expect(screen.getByTestId('div-2')).toHaveTextContent('initialValueTwo');
+});
+
+it('Updates data by listening to other tabs and renders it', async () => {
+  const testMap = persistentMap('test:', {
+    keyOne: 'initialValueOne',
+    keyTwo: 'initialValueTwo'
+  });
+
+  const Wrapper = () => {
+    const $testMap = useStore(testMap);
+
+    return (
+      <>
+        <div data-testid="div-1">{$testMap().keyOne}</div>
+        <div data-testid="div-2">{$testMap().keyTwo}</div>
+        <button
+          onClick={() => {
+            window.dispatchEvent(
+              new StorageEvent('storage', {
+                key: 'test:keyOne',
+                newValue: 'emittedValueOne'
+              })
+            );
+          }}
+        >
+          Emit One
+        </button>
+        <button
+          onClick={() => {
+            window.dispatchEvent(
+              new StorageEvent('storage', {
+                key: 'test:keyTwo',
+                newValue: 'emittedValueTwo'
+              })
+            );
+          }}
+        >
+          Emit Two
+        </button>
+      </>
+    );
+  };
+
+  render(() => <Wrapper />);
+  expect(screen.getByTestId('div-1')).toHaveTextContent('initialValueOne');
+  expect(screen.getByTestId('div-2')).toHaveTextContent('initialValueTwo');
+  expect(localStorage.getItem('test:keyOne')).toEqual('initialValueOne');
+  expect(localStorage.getItem('test:keyTwo')).toEqual('initialValueTwo');
+  screen.getAllByRole('button').forEach(button => button.click());
+  expect(localStorage.getItem('test:keyOne')).toEqual('initialValueOne');
+  expect(localStorage.getItem('test:keyTwo')).toEqual('initialValueTwo');
+  expect(screen.getByTestId('div-1')).toHaveTextContent('emittedValueOne');
+  expect(screen.getByTestId('div-2')).toHaveTextContent('emittedValueTwo');
+});
+
+it('Should not update data by listening to other tabs if disabled and continues rendering existing data', async () => {
+  const testMap = persistentMap(
+    'test:',
+    {
+      keyOne: 'initialValueOne',
+      keyTwo: 'initialValueTwo'
+    },
+    { listen: false }
+  );
+
+  const Wrapper = () => {
+    const $testMap = useStore(testMap);
+
+    return (
+      <>
+        <div data-testid="div-1">{$testMap().keyOne}</div>
+        <div data-testid="div-2">{$testMap().keyTwo}</div>
+        <button
+          onClick={() => {
+            window.dispatchEvent(
+              new StorageEvent('storage', {
+                key: 'test:keyOne',
+                newValue: 'emittedValueOne'
+              })
+            );
+          }}
+        >
+          Emit One
+        </button>
+        <button
+          onClick={() => {
+            window.dispatchEvent(
+              new StorageEvent('storage', {
+                key: 'test:keyTwo',
+                newValue: 'emittedValueTwo'
+              })
+            );
+          }}
+        >
+          Emit Two
+        </button>
+      </>
+    );
+  };
+
+  render(() => <Wrapper />);
+  expect(screen.getByTestId('div-1')).toHaveTextContent('initialValueOne');
+  expect(screen.getByTestId('div-2')).toHaveTextContent('initialValueTwo');
+  expect(localStorage.getItem('test:keyOne')).toEqual('initialValueOne');
+  expect(localStorage.getItem('test:keyTwo')).toEqual('initialValueTwo');
+  screen.getAllByRole('button').forEach(button => button.click());
+  expect(localStorage.getItem('test:keyOne')).toEqual('initialValueOne');
+  expect(localStorage.getItem('test:keyTwo')).toEqual('initialValueTwo');
+  expect(screen.getByTestId('div-1')).toHaveTextContent('initialValueOne');
+  expect(screen.getByTestId('div-2')).toHaveTextContent('initialValueTwo');
+});
